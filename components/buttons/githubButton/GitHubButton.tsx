646a047ad487100f4e9dcc2ed5f6701637b299a7
Nathan Gomes <nahtann@outlook.com>
2021-08-10 18:53:06 -0300

    github button changed

--- components/buttons/githubButton/GitHubButton.tsx
+++ components/buttons/githubButton/GitHubButton.tsx
@@ -4,13 +4,14 @@ import { FaGithub } from 'react-icons/fa'
 
 const GitHubButton = () => {
   return (
-    <button 
+    <a
       className={`${PatterStyle.button} ${styles.githubButton}`}
-      
+      href="https://github.com/NahtanN"
+      target="_blank"
     >
       <p>GitHub</p>
       <FaGithub size={22} id={styles.github} />
-    </button>
+    </a>
   )
 }
 
