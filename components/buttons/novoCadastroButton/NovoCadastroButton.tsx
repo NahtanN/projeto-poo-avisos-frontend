aad64347039d3e4ba89eab9888e716dd4f049dde
Nathan Gomes <nahtann@outlook.com>
2021-08-10 19:12:52 -0300

    criar-aviso page created

--- components/buttons/novoCadastroButton/NovoCadastroButton.tsx
+++ components/buttons/novoCadastroButton/NovoCadastroButton.tsx
@@ -1,11 +1,24 @@
+import Link from 'next/link'
+
 import PatterStyle from '../PatternStyle.module.css'
 import styles from './NovoCadastroButton.module.css'
 
-const NovoCadastroButton = () => {
+export interface IButtonLayout {
+  title: string;
+  route: string;
+}
+
+const NovoCadastroButton = ({ title, route }: IButtonLayout) => {
   return (
-    <button className={`${PatterStyle.button} ${styles.cadastroButton}`}>
-      <p>Cadastrar novo aviso</p>
-    </button>
+
+    <Link
+      href={route}
+    >
+      <a className={`${PatterStyle.button} ${styles.cadastroButton}`}>
+        <p>{ title }</p>
+      </a>
+    </Link>
+
   )
 }
 
--- components/buttonsLayout/ButtonsLayout.tsx
+++ components/buttonsLayout/ButtonsLayout.tsx
@@ -1,13 +1,13 @@
 import styles from './ButtonsLayout.module.css'
 import { GitHubButton } from '../buttons/githubButton/GitHubButton'
-import { NovoCadastroButton } from '../buttons/novoCadastroButton/NovoCadastroButton'
+import { NovoCadastroButton, IButtonLayout } from '../buttons/novoCadastroButton/NovoCadastroButton'
 
-const ButtonsLayout = () => {
+const ButtonsLayout = ({ title, route }: IButtonLayout) => {
   return (
     <div className={styles.buttonsDiv}>
 
       <GitHubButton />
-      <NovoCadastroButton />
+      <NovoCadastroButton title={title} route={route}/>
 
     </div>
   )
